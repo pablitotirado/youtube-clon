55476d5aae0716c488c0fdd14dfb6f159705dd4b
Pab-Source <pablo.tirado.toledo@gmail.com>
2020-06-08 20:54:06 -0300

    add normalize and styles-global

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const Titulo = styled.h1`
   color: red;
--- store.js
+++ store.js
@@ -2,7 +2,15 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 
-const reducer = combineReducers({});
+function reducerPrueba(state = {}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+const reducer = combineReducers({
+  reducerPrueba,
+});
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
