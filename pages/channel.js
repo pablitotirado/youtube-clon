60606320300857a4432fcbba72622901f237b503
Pab-Source <pablo.tirado.toledo@gmail.com>
2020-06-30 22:38:49 -0300

    add logout and others

---
+++ pages/channel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Channel = () => {
+	return (
+		<>
+			<h1>hola Channel</h1>
+		</>
+	);
+};
+
+export default Channel;
---
+++ pages/playlist.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlayList = () => {
+	return (
+		<>
+			<h1>hola playlist</h1>
+		</>
+	);
+};
+
+export default PlayList;
